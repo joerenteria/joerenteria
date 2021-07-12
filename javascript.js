55548ecb999e89bb011c3eb974f9c6c27027d434
Joe Renteria <joe@electronflight.com>
2021-07-12 11:39:45 -0700

    js update

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const title = document.createElement(`h2`);
-title.textContent = "Title of Page";
-
-document.getElementsByClassName('header').appendChild(title);
+let headTag = document.createElement("h1");                 // Create a <li> node
+let headText = document.createTextNode("Joe Renteria");         // Create a text node
+headTag.appendChild(headText);                              // Append the text to <li>
+document.getElementsByClassName("header").appendChild(headTag);     // Append <li> to <ul> with id="myList"
