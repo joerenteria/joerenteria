9350f8d8860774aebcfce6786ab56629ec604ba5
Joe Renteria <joe@electronflight.com>
2021-07-11 17:57:48 -0700

    title

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const title = document.createElement(h2);
+const title = document.createElement(`h2`);
 title.textContent = "Title of Page";
 
 document.getElementsByClassName(header).appendChild(title);
